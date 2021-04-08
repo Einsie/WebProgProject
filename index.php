8e959969402c0e110204ce3f4786869d58cc76a1
Einstainer <32267134+Einstainer@users.noreply.github.com>
2021-04-08 23:47:04 +0300

    aa

--- index.php
+++ index.php
@@ -7,21 +7,17 @@
         <title>Peliyhteisö</title>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"> 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="/./style/custom.css">
         <link rel="stylesheet" href="/./style/site.css">
         
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-        <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script> 
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        <script src="/./js/date.js"></script> 
+        <script src="/./js/myjs.js"></script> 
         <link rel="icon" type="image/x-icon" href="/./favicon.ico">
     </head>
 </head>
 
 <body class="custom-body">
-
 
     <nav class="navbar">
         <div id="logo">
