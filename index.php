3088e39d56786c6c6376d24526e371da5f911106
Einstainer <32267134+Einstainer@users.noreply.github.com>
2021-04-08 22:02:14 +0300

    header info
    Albert

--- index.php
+++ index.php
@@ -4,8 +4,20 @@
 <head>
     <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>asemointiharjoitus</title>
+        <title>Peliyhteisö</title>
         <link rel="stylesheet" href="site.css">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">  
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="/./style/custom.css">
+        <link rel="stylesheet" href="/./style/adds.css">
+        
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script> 
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <script src="/./js/date.js"></script> 
+        <link rel="icon" type="image/x-icon" href="/./favicon.ico">
     </head>
 </head>
 
