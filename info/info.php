e8fe03e77fbdb1325fb3bb91d029c40fc66de7cc
Einstainer <32267134+Einstainer@users.noreply.github.com>
2021-04-09 05:01:09 +0300

    Fixed paths
    Albert

--- info/info.php
+++ info/info.php
@@ -7,13 +7,13 @@
         <title>Peliyhteisö</title>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"> 
-        <link rel="stylesheet" href="/./moodle/phpProjekti/style/site.css">
+        <link rel="stylesheet" href="/./style/site.css">
 
 
 
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-        <script src="/./moodle/phpProjekti/js/myjs.js"></script> 
+        <script src="/./js/myjs.js"></script> 
     </head>
 </head>
 
@@ -21,7 +21,7 @@
 
     <nav class="navbar navbar-expand-md position-sticky navbar-light">
         <div class="navbar-brand nav-link">
-            <h1><a href="/./moodle/phpProjekti/index.php">Peliyhteisö</a></h1>
+            <h1><a href="/./index.php">Peliyhteisö</a></h1>
         </div>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapse" aria-expanded="false" aria-controls="multiCollapse" >
             <span class="navbar-toggler-icon"></span>
@@ -29,13 +29,13 @@
 
         <div class="collapse navbar-collapse justify-content-between" id="multiCollapse">
             <div class="navbar-nav">
-                <a class= "nav-item dropdown-item" href="/./moodle/phpProjekti/info/info.php">Info</a>  
-                <a class= "nav-item dropdown-item" href="/./moodle/phpProjekti/games/games.php">Pelit</a>
-                <a class= "nav-item dropdown-item" href="/./moodle/phpProjekti/feedback/feedback.php">Palaute</a>
+                <a class= "nav-item dropdown-item" href="/./info/info.php">Info</a>  
+                <a class= "nav-item dropdown-item" href="/./games/games.php">Pelit</a>
+                <a class= "nav-item dropdown-item" href="/./feedback/feedback.php">Palaute</a>
             </div>
 
             <div class="navbar-nav">
-                    <a class= "nav-item dropdown-item" href="/./moodle/phpProjekti/signup/signup.php">Rekisteröidy</a>    
+                    <a class= "nav-item dropdown-item" href="/./signup/signup.php">Rekisteröidy</a>    
             </div>
         </div>
     </nav>
