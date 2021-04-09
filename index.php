213327cd34f66a140f825411b5383471e14f37ea
Einstainer <32267134+Einstainer@users.noreply.github.com>
2021-04-09 03:51:35 +0300

    Big update

--- index.php
+++ index.php
@@ -7,53 +7,62 @@
         <title>Peliyhteisö</title>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"> 
-        <link rel="stylesheet" href="/./style/custom.css">
         <link rel="stylesheet" href="/./style/site.css">
 
 
+
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
         <script src="/./js/myjs.js"></script> 
-        <link rel="icon" type="image/x-icon" href="/./favicon.ico">
     </head>
 </head>
 
-<body class="custom-body">
+<body class="customaBody">
 
-    <nav class="navbar">
-        <div class="logo">
+    <nav class="navbar navbar-expand-md position-sticky navbar-light">
+        <div class="navbar-brand nav-link">
             <h1><a href="/./index.php">Peliyhteisö</a></h1>
         </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapse" aria-expanded="false" aria-controls="multiCollapse" >
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-        <div class="navcontent">
-            <div class="navi">
-                <a href="/./feedback/feedback.php" class= "navlink">info</a>
-                <a href="/./info/info.php" class= "navlink">palaute</a>  
+        <div class="collapse navbar-collapse justify-content-between" id="multiCollapse">
+            <div class="navbar-nav">
+                <a class= "nav-item dropdown-item" href="/./feedback/feedback.php">Info</a>
+                <a class= "nav-item dropdown-item" href="/./info/info.php">Palaute</a>
+                <a class= "nav-item dropdown-item" href="/./games/games.php">Pelit</a>  
             </div>
 
-            <div class="navi">
-                    <a href="/./signup/signup.php" class= "navlink">Rekisteröidy</a>    
+            <div class="navbar-nav">
+                    <a class= "nav-item dropdown-item" href="/./signup/signup.php">Rekisteröidy</a>    
             </div>
         </div>
     </nav>
+    <div> 
 
+    </div>
 
-    <div id="sailio">
-        <div id="sisalto">
+    <div class="container" id="firstContainer">
+        <div class="row">
+            <h1>Lorema ispasasasuasm</h1>
+            <div class="col">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius
+                    commodo facilisis. Donec ac elit vitae dui luctus fermentum. Cras non
+                    augue id massa placerat tempus a at leo. Aliquam rutrum sem massa.
+                    Praesent eu adipiscing sapien. Nullam tincidunt laoreet sem eget
+                    rhoncus. Maecenas eleifend lorem non tortor semper sed ultrices tortor
+                    commodo. Suspendisse rhoncus libero eget nibh congue dapibus. Aenean
+                    ornare ipsum eu magna rutrum vulputate. Cras a nibh justo. Aenean luctus
+                    faucibus molestie. Curabitur scelerisque, nulla non lacinia mollis,
+                    purus leo interdum ipsum, sit amet tincidunt sapien orci vitae velit.
+                    Sed non lectus id risus vulputate iaculis. Etiam ante quam, rhoncus ac
+                    ultrices quis, mollis et sapien. Aenean gravida hendrerit justo sed
+                    dictum.
+                </p>
+            </div>
 
-            <h1>Lorem ipsum</h1>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius
-                commodo facilisis. Donec ac elit vitae dui luctus fermentum. Cras non
-                augue id massa placerat tempus a at leo. Aliquam rutrum sem massa.
-                Praesent eu adipiscing sapien. Nullam tincidunt laoreet sem eget
-                rhoncus. Maecenas eleifend lorem non tortor semper sed ultrices tortor
-                commodo. Suspendisse rhoncus libero eget nibh congue dapibus. Aenean
-                ornare ipsum eu magna rutrum vulputate. Cras a nibh justo. Aenean luctus
-                faucibus molestie. Curabitur scelerisque, nulla non lacinia mollis,
-                purus leo interdum ipsum, sit amet tincidunt sapien orci vitae velit.
-                Sed non lectus id risus vulputate iaculis. Etiam ante quam, rhoncus ac
-                ultrices quis, mollis et sapien. Aenean gravida hendrerit justo sed
-                dictum.</p>
+            
         </div>
 
     </div>
--- signup/signup.php
+++ signup/signup.php
@@ -7,62 +7,70 @@
         <title>Peliyhteisö</title>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"> 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="/./style/custom.css">
-        <link rel="stylesheet" href="/./style/adds.css">
-        
+        <link rel="stylesheet" href="/./style/site.css">
+
+
+
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-        <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script> 
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        <script src="/./js/date.js"></script> 
-        <link rel="icon" type="image/x-icon" href="/./favicon.ico">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+        <script src="/./js/myjs.js"></script> 
     </head>
 </head>
 
-<body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+<body class="customaBody">
 
+    <nav class="navbar navbar-expand-md position-sticky navbar-light">
+        <div class="navbar-brand nav-link">
+            <h1><a href="/./index.php">Peliyhteisö</a></h1>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapse" aria-expanded="false" aria-controls="multiCollapse" >
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <nav class="navBar">
-     <div id="navi1">
-            <div>
-                <h1><a href="/./index.php">Peliyhteisö</a></h1>
+        <div class="collapse navbar-collapse justify-content-between" id="multiCollapse">
+            <div class="navbar-nav">
+                <a class= "nav-item dropdown-item" href="/./feedback/feedback.php">Info</a>
+                <a class= "nav-item dropdown-item" href="/./info/info.php">Palaute</a>
+                <a class= "nav-item dropdown-item" href="/./games/games.php">Pelit</a>  
             </div>
 
-            <div>
-               <a href="/./feedback/feedback.php">info</a>
-               <a href="/./info/info.php">palaute</a>
-            </div>
-
-            <div>
-                 <a href="/./signup/signup.php">Rekisteröidy</a>
+            <div class="navbar-nav">
+                    <a class= "nav-item dropdown-item" href="/./signup/signup.php">Rekisteröidy</a>    
             </div>
         </div>
     </nav>
+    <div> 
 
+    </div>
 
-    <div id="sailio">
-        <div id="sisalto">
+    <div class="container" id="firstContainer">
+        <div class="row">
+            <h1>Lorema ispasasasuasm</h1>
+            <div class="col">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius
+                    commodo facilisis. Donec ac elit vitae dui luctus fermentum. Cras non
+                    augue id massa placerat tempus a at leo. Aliquam rutrum sem massa.
+                    Praesent eu adipiscing sapien. Nullam tincidunt laoreet sem eget
+                    rhoncus. Maecenas eleifend lorem non tortor semper sed ultrices tortor
+                    commodo. Suspendisse rhoncus libero eget nibh congue dapibus. Aenean
+                    ornare ipsum eu magna rutrum vulputate. Cras a nibh justo. Aenean luctus
+                    faucibus molestie. Curabitur scelerisque, nulla non lacinia mollis,
+                    purus leo interdum ipsum, sit amet tincidunt sapien orci vitae velit.
+                    Sed non lectus id risus vulputate iaculis. Etiam ante quam, rhoncus ac
+                    ultrices quis, mollis et sapien. Aenean gravida hendrerit justo sed
+                    dictum.
+                </p>
+            </div>
 
-            <h1>Lorem ipsum</h1>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius
-                commodo facilisis. Donec ac elit vitae dui luctus fermentum. Cras non
-                augue id massa placerat tempus a at leo. Aliquam rutrum sem massa.
-                Praesent eu adipiscing sapien. Nullam tincidunt laoreet sem eget
-                rhoncus. Maecenas eleifend lorem non tortor semper sed ultrices tortor
-                commodo. Suspendisse rhoncus libero eget nibh congue dapibus. Aenean
-                ornare ipsum eu magna rutrum vulputate. Cras a nibh justo. Aenean luctus
-                faucibus molestie. Curabitur scelerisque, nulla non lacinia mollis,
-                purus leo interdum ipsum, sit amet tincidunt sapien orci vitae velit.
-                Sed non lectus id risus vulputate iaculis. Etiam ante quam, rhoncus ac
-                ultrices quis, mollis et sapien. Aenean gravida hendrerit justo sed
-                dictum.</p>
+            
         </div>
 
     </div>
 
+
+<footer>
+    
+</footer>
 
 </body>
 
